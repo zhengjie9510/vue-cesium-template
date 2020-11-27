b73ea36926b6839c64aeaac2008b7eb1683133ea
zhengjie9510 <1096318621@qq.com>
2020-11-27 13:37:03 +0800

    Modify configuration

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,32 +5,23 @@
 </template>
 
 <script>
-// 文件 node_modules\vue-cli-plugin-cesium\index.js 中已经全局引入 Cesium，因此无需再次 import
-
-// 也可注释掉全局引用，通过 require 引用
-// var Cesium = require('cesium/Cesium');//To require all of CesiumJS
-// 也可以通过 import 引用
-// import {Viewer} from 'cesium/Cesium';//这里有坑，不能 import Cesium
+var Cesium = require("cesium/Cesium");
+var widgets = require("cesium/Widgets/widgets.css");
 export default {
   name: "",
   mounted() {
     var viewer = new Cesium.Viewer("cesiumContainer");
-
-    var imageryLayers = viewer.imageryLayers;
-    // ArcGIS 影像服务
-    imageryLayers.addImageryProvider(
-      // eslint-disable-next-line no-undef
-      new Cesium.ArcGisMapServerImageryProvider({
-        url:
-          "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer",
-      })
-    );
-
-    // eslint-disable-next-line no-console
-    console.log(viewer);
+    //谷歌影像
+    var googleImagery = new Cesium.UrlTemplateImageryProvider({
+      url:
+        "http://mt{s}.google.cn/vt/lyrs=s&hl=zh-CN&gl=CN&src=app&x={x}&y={y}&z={z}",
+      subdomains: "0123",
+    });
+    viewer.imageryLayers.addImageryProvider(googleImagery);
   },
 };
 </script> 
+
 <style scoped>
 .map-box {
   width: 100%;
